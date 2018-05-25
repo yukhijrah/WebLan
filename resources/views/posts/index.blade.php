a8dc05a33f683c3881eb78f2c1ed854e413378c2
rakhadiasry <rakha.diasry@gmail.com>
2018-05-25 16:11:46 +0700

    buat method destroy

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
-
+ 
 @section('content')
 <div class="container">
     <div class="row">
         <div class="col-md-10">
             <h1>All Posts</h1>
         </div>
-
+ 
         <div class="col-md-2">
             <a href="{{ route('posts.create') }}" class="btn btn-lg btn-block btn-primary btn-h1-spacing">Create New Post</a>
         </div>
@@ -14,7 +14,7 @@
             <hr>
         </div>
     </div> <!-- end of .row -->
-
+ 
     <div class="row">
         <div class="col-md-12">
             <table class="table">
@@ -25,26 +25,36 @@
                     <th>Created At</th>
                     <th></th>
                 </thead>
-
+ 
                 <tbody>
-
+ 
                     @foreach ($posts as $post)
-
+ 
                         <tr>
                             <th>{{ $post->id }}</th>
                             <td>{{ $post->title }}</td>
                             <td>{{ substr(strip_tags($post->content), 0, 50) }}{{ strlen(strip_tags($post->content)) > 50 ? "..." : "" }}</td>
                             <td>{{ date('M j, Y', strtotime($post->created_at)) }}</td>
-                            <td><a href="{{ route('posts.show', $post->id) }}" class="btn btn-default btn-sm">View</a> <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-default btn-sm">Edit</a></td>
+                            <td>
+                                <a href="{{ route('posts.show', $post->id) }}" class="btn btn-default btn-sm">View</a>
+                                <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-default btn-sm">Edit</a>
+                               
+                               
+                                <form method="POST" action="{{route('posts.destroy', $post->id)}}">
+                                        {{ method_field('DELETE') }}
+                                        {{ csrf_field() }}
+                                <button type="submit" class="btn btn-default btn-sm">Delete</button>
+                                </form>
+                            </td>
                         </tr>
-
+ 
                     @endforeach
-
+ 
                 </tbody>
             </table>
-
+ 
         </div>
     </div>
-
+ 
 </div>
 @endsection
